2b51d7528114e5cbaa8a012b661310acbfec3e84
Juan <juanloureiro2@gmail.com>
2021-12-02 16:55:59 -0300

    atualizacao

--- LM35DZ/LM35DZ.ino
+++ LM35DZ/LM35DZ.ino
@@ -1,16 +1,15 @@
 
-const int LM35 = A0; // Define o pino que lera a saída do LM35
-float temperatura; // Variável que armazenará a temperatura medida
+const int LM35 = A0; 
+float temperatura; 
  
-//Função que será executada uma vez quando ligar ou resetar o Arduino
+
 void setup() {
-Serial.begin(9600); // inicializa a comunicação serial
+Serial.begin(9600); 
 }
  
 //Função que será executada continuamente
 void loop() {
 temperatura = (float(analogRead(LM35))*5/(1023))/0.01;
-Serial.print("Temperatura: ");
 Serial.println(temperatura);
 delay(2000);
 }
